91c187f3c3d4a848ced8aa6ac87b6fa65c6fb372
Dan Zaratsian <dtz001@gmail.com>
2018-10-04 17:44:32 -0400

    Update sklearn_script.py

--- sklearn_script.py
+++ sklearn_script.py
@@ -1,3 +1,43 @@
+
+
+#####################################################################################
+#
+#   Google CloudML
+#
+#####################################################################################
+
+
+# List CloudML Models
+gcloud ml-engine models list
+
+
+# Set ENV Variables
+PROJECT_ID=$(gcloud config list project --format "value(core.project)")
+BUCKET_NAME=${PROJECT_ID}-mlengine
+REGION=us-east1
+
+
+# Create a GCS Bucket to use with CloudML
+gsutil mb -l $REGION gs://$BUCKET_NAME
+
+
+
+
+
+
+Specific to GCP, traction with the Red Hat teams has been slow and we could use executive support to get infront of the right teams at Red Hat. We believe that opportunities exist with the product teams.
+
+
+
+There is a $400k GCP opportunity in the POC stage (Global Replicator). Aside from that, traction with the Red Hat teams has been slow and we could use executive support to get infront of the right teams at Red Hat. We believe that opportunities exist with the product teams.
+
+
+
+
+
+
+
+
 ########################################################################################################
 #
 #   Sklean Model
@@ -27,19 +67,31 @@ from sklearn.externals import joblib
 
 # Read local CSV
 def load_rawdata(training_data):
-    ''' training_data is the path to the training data '''
-    header  = ['Date', 'GameID', 'Drive', 'qtr', 'down', 'time', 'TimeUnder', 'TimeSecs', 'PlayTimeDiff', 'yrdline100', 'ydstogo', 'ydsnet', 'FirstDown', 'posteam', 'DefensiveTeam', 'Yards_Gained', 'Touchdown', 'PlayType', 'PassLength', 'PassLocation', 'RunLocation', 'PosTeamScore', 'DefTeamScore', 'month_day', 'PlayType_lag']
-    rawdata = pd.read_csv(training_data , names=header)
-    
-    # Quickly explore data structure
-    rawdata.head()
-    #rawdata.values
-    rawdata.iloc[0]
-    rawdata.shape
-    rawdata.columns
-    rawdata.dtypes
-    rawdata.describe()
-    return rawdata
+    '''
+        USAGE:
+            rawdata = load_rawdata(training_data='./data/nfldata2.csv')
+            rawdata = load_rawdata(training_data='/tmp/nfldata2.csv')
+    '''
+    try:
+        training_data = re.sub('^\.' , os.getcwd() , training_data)
+        print('[ INFO ] Reading in NFL Training data from from {}'.format(training_data))
+        time.sleep(3)
+        
+        header  = ['Date', 'GameID', 'Drive', 'qtr', 'down', 'time', 'TimeUnder', 'TimeSecs', 'PlayTimeDiff', 'yrdline100', 'ydstogo', 'ydsnet', 'FirstDown', 'posteam', 'DefensiveTeam', 'Yards_Gained', 'Touchdown', 'PlayType', 'PassLength', 'PassLocation', 'RunLocation', 'PosTeamScore', 'DefTeamScore', 'month_day', 'PlayType_lag']
+        rawdata = pd.read_csv(training_data , names=header)
+        # Quickly explore data structure
+        rawdata.head()
+        #rawdata.values
+        rawdata.iloc[0]
+        rawdata.shape
+        rawdata.columns
+        rawdata.dtypes
+        rawdata.describe()
+        print('[ INFO ] Read in training_data located at {}'.format(training_data))
+        return rawdata
+    except:
+        print('[ ERROR ] Could not find training_data. Check directory path and filename, then try again.')
+        #sys.exit()
 
 ########################################################################################################
 #
@@ -47,11 +99,11 @@ def load_rawdata(training_data):
 #
 ########################################################################################################
 
-def transform_df(rawdata):
+def transform_df(rawdata, target_variable_name):
     
     # Model Variables (Specify id, target, numeric variables, and categorical variables)
     var_id              = ''
-    var_target          = 'Yards_Gained'
+    var_target          = target_variable_name #'Yards_Gained'
     var_date            = 'Date'
     var_numeric         = ['Drive', 'qtr', 'down', 'TimeSecs', 'PlayTimeDiff', 'yrdline100', 'ydstogo', 'ydsnet', 'FirstDown', 'PosTeamScore', 'DefTeamScore', 'month_day', ]
     var_category        = ['posteam', 'DefensiveTeam','PlayType','PlayType_lag']
@@ -92,7 +144,7 @@ def transform_df(rawdata):
 #
 ########################################################################################################
 
-def partition_df(transformed_df):
+def partition_df(transformed_df, target_variable_name):
     
     random_number  = pd.DataFrame(np.random.randn(len(transformed_df), 1))
     partition_mask = np.random.rand(len(random_number)) <= 0.75
@@ -103,11 +155,11 @@ def partition_df(transformed_df):
     train_data.shape
     test_data.shape
     
-    train_inputs   = train_data.drop([var_target], axis=1)
-    train_target   = train_data[var_target]
+    train_inputs   = train_data.drop([target_variable_name], axis=1)
+    train_target   = train_data[target_variable_name]
     
-    test_inputs    = test_data.drop([var_target], axis=1)
-    test_target    = test_data[var_target]
+    test_inputs    = test_data.drop([target_variable_name], axis=1)
+    test_target    = test_data[target_variable_name]
     
     return train_inputs, train_target, test_inputs, test_target
 
@@ -158,22 +210,22 @@ def save_model(model_obj):
 if __name__ == "__main__"
     
     # Arguments (used only for testing)
-    #args = {"training_data":"", "target_variable"=""}
+    #args = {"training_data":"./data/nfldata2.csv", "target_variable_name":"Yards_Gained"}
     
     # Arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("--training_data",      required=True, type=str, help="Path to local training data")
-    ap.add_argument("--target_variable",    required=True, type=str, help="Name of target / label variable")
+    ap.add_argument("--training_data",          required=True, type=str, help="Path to local training data")
+    ap.add_argument("--target_variable_name",   required=True, type=str, help="Name of target / label variable")
     args = vars(ap.parse_args())
     
     # Load Dataset
     rawdata = load_rawdata(args['training_data'])
     
     # Transform / Prep dataframe
-    transformed_df = transform_df(rawdata)
+    transformed_df = transform_df(rawdata, args['target_variable_name'])
     
     # Split into train and test dataframes
-    train_inputs, train_target, test_inputs, test_target = partition_df(transformed_df)
+    train_inputs, train_target, test_inputs, test_target = partition_df(transformed_df, args['target_variable_name'])
     
     # Train Model
     model_obj, target_predicted = train_model(train_inputs, train_target, test_inputs, number_of_estimators=100)
