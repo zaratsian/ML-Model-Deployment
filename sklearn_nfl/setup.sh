88fb6e880428aba8efea63c309fdc8d6a5085d37
Dan Zaratsian <dzaratsian@google.com>
2019-04-16 21:06:26 -0400

    added setup

---
+++ sklearn_nfl/setup.sh
@@ -0,0 +1,24 @@
+##################################################################
+#
+#  Environment Initial Setup
+#
+##################################################################
+
+VIRTUAL_ENV_NAME=cloud_ml
+
+##################################################################
+#
+# Create Conda Virtual Env 
+# NOTE: This only needs to be ran one time to get the venv setup
+#
+##################################################################
+
+echo "[ INFO ] Creating conda virtual env ($VIRTUAL_ENV_NAME)"
+conda create --name $VIRTUAL_ENV_NAME python=3.5 \
+    numpy=1.14.5 \
+    pandas=0.23.3 \
+    scipy=1.1.0 \
+    scikit-learn=0.19.2
+
+
+#ZEND
